a04cd8c71fc1e7d0f1c6c852976e1d84887172a7
Michael Alls <allsjr@gmail.com>
2022-01-21 14:10:45 -0500

    added code to hit notion pay db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { Client } from "@notionhq/client/build/src"
+const notion = new Client({auth: process.env.NOTION_KEY});
+
+export default async function handler(req, res) {
+  // res.status(200).json({ name: 'John Doe' })
+
+  const response = await notion.databases.query({database_id: process.env.NOTION_DB_ID});
+
+  res.status(200).json(response);
+
 }
