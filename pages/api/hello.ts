c9af3f0c5a34fb77638d224b7e17b253ef503415
Michael Alls <allsjr@gmail.com>
2022-01-22 10:54:17 -0500

    fixing typescript issues

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { Client } from '@notionhq/client/build/src';
 const notion = new Client({ auth: process.env.NOTION_KEY });
-
-export default async function handler(req, res) {
+/**
+ *
+ * @param {NextApiRequest} req
+ * @param {NextApiResponse} res
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // res.status(200).json({ name: 'John Doe' })
 
   const response = await notion.databases.query({
-    database_id: process.env.NOTION_DB_ID,
+    database_id: process.env.NOTION_DB_ID || '',
   });
 
   res.status(200).json(response);
